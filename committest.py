9ce5012d1e1c00f4884bd1c3f46cc882e37d8686
Antonio Dell'Elce <adellelce@hotmail.com>
2013-04-18 23:00:59 +0200

    testing an issue with commit

---
+++ committest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# File:         committest.py
+# Created:      2258 180413
+# Description:  description for committest.py
+#
+#
+
+#testing a commit issue...
+
+class commitTest:
+	pass
+
+
+print 'commit test'
+
+
+## EOF ##
+
